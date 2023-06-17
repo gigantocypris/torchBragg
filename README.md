98e8a21950957e921ec3fd43697e2806e22fbef9
vganapati <vganapa1@swarthmore.edu>
2023-06-16 18:51:29 -0700

    single image simulation

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # torchBragg
+
 Structure factor refinement integrating Careless and nanoBragg
+
+## How to run a forward simulation with nanoBragg
+
+Use the [exafel_project](https://github.com/ExaFEL/exafel_project) repository.
+```
+git clone https://github.com/ExaFEL/exafel_project.git
+git checkout experimental_high_remote
+```
+
+Login to Perlmutter, source your environment, and create a simulated image:
+```
+source ~\env_p20231_2
+cd $MODULES
+cd ../
+. activate.sh
+mkdir $WORK/output_torchBragg
+cd $WORK/output_torchBragg
+> sbatch $MODULES/exafel_project/kpp-sim/sim_ferredoxin_high_remote.sh
+```
---
+++ single_img_sim.sh
@@ -0,0 +1,43 @@
+export CCTBX_DEVICE_PER_NODE=1
+export N_START=0
+export LOG_BY_RANK=1 # Use Aaron's rank logger
+export RANK_PROFILE=0 # 0 or 1 Use cProfiler, default 1
+export N_SIM=1 # total number of images to simulate
+export ADD_BACKGROUND_ALGORITHM=cuda
+export DEVICES_PER_NODE=4
+export MOS_DOM=25
+
+export CCTBX_NO_UUID=1
+export DIFFBRAGG_USE_KOKKOS=1
+export CUDA_LAUNCH_BLOCKING=1
+export NUMEXPR_MAX_THREADS=128
+export SLURM_CPU_BIND=cores # critical to force ranks onto different cores. verify with ps -o psr <pid>
+export OMP_PROC_BIND=spread
+export OMP_PLACES=threads
+export SIT_PSDM_DATA=/global/cfs/cdirs/lcls/psdm-sauter
+export CCTBX_GPUS_PER_NODE=1
+export XFEL_CUSTOM_WORKER_PATH=$MODULES/psii_spread/merging/application # User must export $MODULES path
+
+echo "
+noise=True
+psf=False
+attenuation=True
+context=kokkos_gpu
+absorption=high_remote
+beam {
+  mean_energy=9500.
+}
+spectrum {
+  nchannels=100
+  channel_width=1.0
+}
+detector {
+  tiles=multipanel
+  reference=$MODULES/exafel_project/kpp-sim/t000_rg002_chunk000_reintegrated_000000.expt
+}
+output {
+  format=h5
+}
+" > trial.phil
+
+libtbx.python $MODULES/exafel_project/kpp_utils/LY99_batch.py trial.phil
