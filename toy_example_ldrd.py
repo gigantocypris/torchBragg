a00a547c45dffa93a5c953d218ca4e72fd3a952a
Vidya Ganapati <vganapati@gmail.com>
2023-12-01 16:14:59 -0800

    ldrd toy example

--- toy_example_ldrd.py
+++ toy_example_ldrd.py
@@ -1,4 +1,9 @@
+"""
+cd $WORK/output_torchBragg
+libtbx.python $MODULES/torchBragg/toy_example_ldrd.py
+"""
 import torch
+import numpy as np
 
 def model(x, coeff_0, coeff_1, operations):
     x_0, x_1 = x
@@ -8,34 +13,61 @@ def model(x, coeff_0, coeff_1, operations):
     z_1 = operations[2](y_0, y_1)
     return(torch.stack([z_0, z_1], axis=0))
 
-def data_fit(x, operations, observations):
-    z = model(x, operations)
-    return(torch.sum((z-observations)**2))
+def data_fit(x, coeff_0, coeff_1, operations, observations):
+    z = model(x, coeff_0, coeff_1,operations)
+    return(torch.mean((z-observations)**2))
 
 if __name__ == '__main__':
+    let_coeff_vary = True
+
     x_0_vec = torch.arange(0, 10, 1)
     x_1_vec = torch.arange(0, 10, 1)
 
-    x0_m, x1_m = torch.meshgrid(x_0_vec, x_1_vec)
+    num_iter = 1000
+
+    x0_m, x1_m = torch.meshgrid(x_0_vec, x_1_vec, indexing='ij')
     ground_truth_x = torch.stack([x0_m, x1_m], axis=2)
     ground_truth_x = ground_truth_x.reshape(2, -1)
-    ground_truth_operations = [torch.multiply, torch.multiply, torch.multiply]
-    ground_truth_coeff_0 = torch.Tensor([4])
-    ground_truth_coeff_1 = torch.Tensor([3])
+    ground_truth_operations = [torch.multiply, torch.multiply, torch.add]
+    ground_truth_coeff_0 = torch.Tensor([5])
+    ground_truth_coeff_1 = torch.Tensor([4])
     observed_z = model(ground_truth_x, ground_truth_coeff_0, ground_truth_coeff_1, ground_truth_operations)
 
-    assumed_coeff_vec = torch.repeat_interleave([[4],[3]], ground_truth_x.shape[1], axis=1)
     assumed_operations = [torch.multiply, torch.multiply, torch.add]
 
-    # find the x that fits the observed z, given assumed coeff and operations
+    # find the x that fits the observed z
 
-    # initial conditions
-    x_0 = torch.ones([2, observed_z.shape[1]])*5
+    if let_coeff_vary:
+        # allow the coefficients to vary
+        x_0 = torch.tensor(np.ones([2, observed_z.shape[1]])*5, requires_grad=True)
+        assumed_coeff_0 = torch.tensor(4., requires_grad=True)
+        assumed_coeff_1 = torch.tensor(3., requires_grad=True)
 
-    # allow the coefficients to vary
+        optimizer = torch.optim.Adam([x_0, assumed_coeff_0, assumed_coeff_1], lr=0.01)
+        # optimizer = torch.optim.LBFGS([x_0, assumed_coeff_0, assumed_coeff_1], lr=0.0001)
+    else:
+        # do not allow the coefficients to vary
+        x_0 = torch.tensor(np.ones([2, observed_z.shape[1]])*5, requires_grad=True)
+        assumed_coeff_0 = torch.tensor(4., requires_grad=False)
+        assumed_coeff_1 = torch.tensor(3., requires_grad=False)
 
+        optimizer = torch.optim.Adam([x_0], lr=0.01)
+        # optimizer = torch.optim.LBFGS([x_0], lr=0.0001)
+
+    for i in range(num_iter):
+        def closure():
+            optimizer.zero_grad()
+            loss = data_fit(x_0, assumed_coeff_0, assumed_coeff_1, assumed_operations, observed_z)
+            loss.backward()
+            return loss
+        optimizer.step(closure)
+    
+    print(closure())
+    print(assumed_coeff_0.detach().numpy(), assumed_coeff_1.detach().numpy())
+
+
+
+    
     # do not allow the coefficients to vary
 
     # try all sets of operations
-
-    breakpoint()
